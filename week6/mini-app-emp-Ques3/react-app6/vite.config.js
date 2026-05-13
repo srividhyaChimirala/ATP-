faa8309d0a956878f00ccfc8821b131273305dcb
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-13 20:31:58 +0530

    Update vite.config.js

--- week6/mini-app-emp-Ques3/react-app6/vite.config.js
+++ week6/mini-app-emp-Ques3/react-app6/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),
+    tailwindcss()
   ],
-})
-
-
-
+  server: {
+    allowedHosts: "all"
+  }
+});
