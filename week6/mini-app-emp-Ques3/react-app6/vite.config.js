b5efb43e7da9a11d2417d12a8052babd086b9f54
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-14 13:15:14 +0530

    Update vite.config.js

--- week6/mini-app-emp-Ques3/react-app6/vite.config.js
+++ week6/mini-app-emp-Ques3/react-app6/vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+
+//   preview: {
+//     allowedHosts: "all"
+//   }
+// });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
   preview: {
-    allowedHosts: "all"
+    allowedHosts: ['atp-10.onrender.com']
   }
-});
+})
