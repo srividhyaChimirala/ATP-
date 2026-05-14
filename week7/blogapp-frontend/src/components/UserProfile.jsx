ca2555d3c8baa49819ce32c6d22ae200f134e229
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-14 18:52:13 +0530

    Update UserProfile.jsx

--- week7/blogapp-frontend/src/components/UserProfile.jsx
+++ week7/blogapp-frontend/src/components/UserProfile.jsx
@@ -28,7 +28,7 @@ function UserProfile() {
       setLoading(true);
       try {
         //read articles of all authors
-        let res=await axios.get("http://localhost:4000/user-api/articles",{withCredentials:true})
+        let res=await axios.get("https://atp-13.onrender.com//user-api/articles",{withCredentials:true})
         //update articles state
         if(res.status===200){
           setArticles((await res).data.payload)
