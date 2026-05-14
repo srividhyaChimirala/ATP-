00c4318c04d7a1dd7eafd6f80a6e43810a8fad8a
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-14 18:49:58 +0530

    Update AuthorArticles.jsx

--- week7/blogapp-frontend/src/components/AuthorArticles.jsx
+++ week7/blogapp-frontend/src/components/AuthorArticles.jsx
@@ -33,7 +33,7 @@ function AuthorArticles() {
       try {
         setLoading(true);
         //read articles of current author
-        let res = await axios.get(" http://localhost:4000/author-api/articles", { withCredentials: true });
+        let res = await axios.get(" https://atp-13.onrender.com/author-api/articles", { withCredentials: true });
         if (res.status === 200) {
           setArticles(res.data.payload);
         }
