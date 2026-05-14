13084d70ce994e052cfa8a458efc96d79d7b91e3
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-14 18:52:55 +0530

    Update WriteArticles.jsx

--- week7/blogapp-frontend/src/components/WriteArticles.jsx
+++ week7/blogapp-frontend/src/components/WriteArticles.jsx
@@ -38,7 +38,7 @@ function WriteArticles() {
       //set loading true
       setLoading(true);
       //make POST req to save new article
-      let res = await axios.post("http://localhost:4000/author-api/article", articleObj, { withCredentials: true });
+      let res = await axios.post("https://atp-13.onrender.com/author-api/article", articleObj, { withCredentials: true });
       //navigate to AuthorArticles
       if (res.status === 201) {
         toast.success("Article published successfully")
