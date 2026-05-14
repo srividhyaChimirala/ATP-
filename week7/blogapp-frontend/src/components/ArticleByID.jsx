5f0008fcc9b19050aa46c44809f837fd5a44e639
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-14 18:48:31 +0530

    Update ArticleByID.jsx

--- week7/blogapp-frontend/src/components/ArticleByID.jsx
+++ week7/blogapp-frontend/src/components/ArticleByID.jsx
@@ -50,7 +50,7 @@ function ArticleByID() {
       setLoading(true);
 
       try {
-        const res = await axios.get(`http://localhost:4000/user-api/article/${id}`, { withCredentials: true });
+        const res = await axios.get(`https://atp-13.onrender.com/user-api/article/${id}`, { withCredentials: true });
 
         setArticle(res.data.payload);
       } catch (err) {
@@ -114,7 +114,7 @@ function ArticleByID() {
     //add artcileId
     commentObj.articleId = article._id;
     console.log(commentObj);
-    let res = await axios.put("http://localhost:4000/user-api/articles", commentObj, { withCredentials: true });
+    let res = await axios.put("https://atp-13.onrender.com/user-api/articles", commentObj, { withCredentials: true });
     if (res.status === 200) {
       
       setArticle(res.data.payload);
