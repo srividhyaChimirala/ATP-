5b2d337fc7bf0d352678bca110714acc6ead08c5
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-14 15:15:13 +0530

    Update AuthorApi.js

--- week7/blogapp-backend/APIs/AuthorApi.js
+++ week7/blogapp-backend/APIs/AuthorApi.js
@@ -2,7 +2,7 @@
 import exp from "express";
 import { UserModel } from "../models/UserModel.js";
 import { ArticleModel } from "../models/ArticleModel.js";
-import { verifyToken } from "../middlewares/VerifyToken.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 export const authorApp = exp.Router();
 
