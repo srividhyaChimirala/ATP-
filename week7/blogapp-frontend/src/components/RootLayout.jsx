f2d0c65ef417b46c8a6823f434532977128aa3f8
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-15 13:44:46 +0530

    Update RootLayout.jsx

--- week7/blogapp-frontend/src/components/RootLayout.jsx
+++ week7/blogapp-frontend/src/components/RootLayout.jsx
@@ -1,27 +1,57 @@
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import { Outlet } from "react-router";
+// import {useEffect} from 'react'
+// import { useAuth } from "../store/authStore";
+
+
+
+// function RootLayout() {
+
+//   //import check checkAuth
+//   let checkAuth=useAuth(state=>state.checkAuth)
+
+//   useEffect(()=>{
+//     checkAuth()
+//   },[])
+
+
+//   return (
+//     <div>
+//       <Header />
+//         <div className="min-h-screen mx-32">
+//           <Outlet />
+//         </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default RootLayout;
+
 import Header from "./Header";
 import Footer from "./Footer";
-import { Outlet } from "react-router";
-import {useEffect} from 'react'
+import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import { useAuth } from "../store/authStore";
-
-
 
 function RootLayout() {
 
-  //import check checkAuth
-  let checkAuth=useAuth(state=>state.checkAuth)
+  // import checkAuth
+  const checkAuth = useAuth((state) => state.checkAuth);
 
-  useEffect(()=>{
-    checkAuth()
-  },[])
-
+  useEffect(() => {
+    checkAuth();
+  }, []);
 
   return (
     <div>
       <Header />
-        <div className="min-h-screen mx-32">
-          <Outlet />
-        </div>
+
+      <div className="min-h-screen mx-32">
+        <Outlet />
+      </div>
+
       <Footer />
     </div>
   );
