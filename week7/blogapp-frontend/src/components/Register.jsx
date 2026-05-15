87b302192fc8d45ae6be116b76f1748d8742b101
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-15 13:05:49 +0530

    Update Register.jsx

--- week7/blogapp-frontend/src/components/Register.jsx
+++ week7/blogapp-frontend/src/components/Register.jsx
@@ -11,7 +11,8 @@ import {
   mutedText,
 } from "../styles/common";
 import { useForm } from "react-hook-form";
-import { NavLink, useNavigate } from "react-router";
+//import { NavLink, useNavigate } from "react-router";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 
