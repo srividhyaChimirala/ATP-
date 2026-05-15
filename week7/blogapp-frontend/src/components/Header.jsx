56cd38c2c5463896c30b2f9ae66db638ce837a1f
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-15 09:29:04 +0530

    Update Header.jsx

--- week7/blogapp-frontend/src/components/Header.jsx
+++ week7/blogapp-frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router";
+// import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
 import { useAuth } from "../store/authStore";
 import {
   navbarClass,
