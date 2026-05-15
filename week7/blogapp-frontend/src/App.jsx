14b8e347e2baae24c72c8b6fa33ece1db1bd5f58
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-15 13:04:38 +0530

    Update App.jsx

--- week7/blogapp-frontend/src/App.jsx
+++ week7/blogapp-frontend/src/App.jsx
@@ -1,5 +1,96 @@
-// import { createBrowserRouter, RouterProvider } from "react-router";
+// // import { createBrowserRouter, RouterProvider } from "react-router";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import RootLayout from "./components/RootLayout";
+// import Home from "./components/Home";
+// import Register from "./components/Register";
+// import Login from "./components/Login";
+// import UserProfile from "./components/UserProfile";
+// import AuthorProfile from "./components/AuthorProfile";
+// import AuthorArticles from "./components/AuthorArticles";
+// import EditArticle from "./components/EditArticle";
+// import WriteArticles from "./components/WriteArticles";
+// import ArticleByID from "./components/ArticleById";
+// import { Toaster } from "react-hot-toast";
+// import Unauthorized from "./components/Unauthorized";
+// import ProtectedRoute from "./components/ProtectedRoute";
+
+// function App() {
+//   const routerObj = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <RootLayout />,
+//       children: [
+//         {
+//           path: "",
+//           element: <Home />,
+//         },
+//         {
+//           path: "register",
+//           element: <Register />,
+//         },
+//         {
+//           path: "login",
+//           element: <Login />,
+//         },
+//         {
+//           path: "user-profile",
+//           element: (
+//             <ProtectedRoute allowedRoles={["USER"]}>
+//               <UserProfile />
+//             </ProtectedRoute>
+//           ),
+//         },
+//         {
+//           path: "author-profile",
+//           element: (
+//             <ProtectedRoute allowedRoles={["AUTHOR"]}>
+//               <AuthorProfile />
+//             </ProtectedRoute>
+//           ),
+
+//           children: [
+//             {
+//               index: true,
+//               element: <AuthorArticles />,
+//             },
+//             {
+//               path: "articles",
+//               element: <AuthorArticles />,
+//             },
+//             {
+//               path: "write-article",
+//               element: <WriteArticles />,
+//             },
+//           ],
+//         },
+//         {
+//           path: "article/:id",
+//           element: <ArticleByID />,
+//         },
+//         {
+//           path: "edit-article",
+//           element: <EditArticle />,
+//         },
+//         {
+//           path: "unauthorized",
+//           element: <Unauthorized />,
+//         },
+//       ],
+//     },
+//   ]);
+
+//   return (
+//     <div>
+//       <Toaster position="top-center" reverseOrder={false} />
+//       <RouterProvider router={routerObj} />
+//     </div>
+//   );
+// }
+
+// export default App;
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import RootLayout from "./components/RootLayout";
 import Home from "./components/Home";
 import Register from "./components/Register";
@@ -10,7 +101,6 @@ import AuthorArticles from "./components/AuthorArticles";
 import EditArticle from "./components/EditArticle";
 import WriteArticles from "./components/WriteArticles";
 import ArticleByID from "./components/ArticleById";
-import { Toaster } from "react-hot-toast";
 import Unauthorized from "./components/Unauthorized";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -21,17 +111,21 @@ function App() {
       element: <RootLayout />,
       children: [
         {
-          path: "",
+          index: true,
           element: <Home />,
         },
+
         {
           path: "register",
           element: <Register />,
         },
+
         {
           path: "login",
           element: <Login />,
         },
+
+        // USER ROUTE
         {
           path: "user-profile",
           element: (
@@ -40,6 +134,8 @@ function App() {
             </ProtectedRoute>
           ),
         },
+
+        // AUTHOR ROUTES
         {
           path: "author-profile",
           element: (
@@ -53,37 +149,66 @@ function App() {
               index: true,
               element: <AuthorArticles />,
             },
+
             {
               path: "articles",
               element: <AuthorArticles />,
             },
+
             {
               path: "write-article",
               element: <WriteArticles />,
             },
           ],
         },
+
+        // ARTICLE DETAILS
         {
           path: "article/:id",
           element: <ArticleByID />,
         },
+
+        // EDIT ARTICLE
         {
-          path: "edit-article",
-          element: <EditArticle />,
+          path: "edit-article/:id",
+          element: (
+            <ProtectedRoute allowedRoles={["AUTHOR"]}>
+              <EditArticle />
+            </ProtectedRoute>
+          ),
         },
+
+        // UNAUTHORIZED
         {
           path: "unauthorized",
           element: <Unauthorized />,
+        },
+
+        // 404 PAGE
+        {
+          path: "*",
+          element: (
+            <div className="text-center mt-20 text-2xl font-bold">
+              404 - Page Not Found
+            </div>
+          ),
         },
       ],
     },
   ]);
 
   return (
-    <div>
-      <Toaster position="top-center" reverseOrder={false} />
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 2000,
+        }}
+      />
+
       <RouterProvider router={routerObj} />
-    </div>
+    </>
   );
 }
 
