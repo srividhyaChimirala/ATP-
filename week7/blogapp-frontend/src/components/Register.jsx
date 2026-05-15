359950f71f5461361d67211fe6d71a95618f2252
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-15 13:06:42 +0530

    Update Register.jsx

--- week7/blogapp-frontend/src/components/Register.jsx
+++ week7/blogapp-frontend/src/components/Register.jsx
@@ -24,7 +24,8 @@ function Register() {
   } = useForm();
   const [loading, setLoading] = useState(false);
   const [apiError, setApiError] = useState(null);
-  const [preview, setPriview] = useState(null);
+  // const [preview, setPriview] = useState(null);
+  const [preview, setPreview]= useState(null);
   const navigate = useNavigate();
 
   //When user registration submitted
