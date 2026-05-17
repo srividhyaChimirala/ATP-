d76893954867a6f8d5f8b5d76123857d6f64d0d0
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-05-17 19:26:33 +0530

    Update Register.jsx

--- week7/blogapp-frontend/src/components/Register.jsx
+++ week7/blogapp-frontend/src/components/Register.jsx
@@ -48,7 +48,7 @@ function Register() {
       //start loading
       setLoading(true);
       //make HTTP POST req to create User in backend
-      let res = await axios.post("http://localhost:4000/auth/users", formData,{withCredentials:true});
+      let res = await axios.post("https://blogapp-m811.onrender.com/auth/users", formData,{withCredentials:true});
 
       if (res.status === 201) {
         //navigate to Login
